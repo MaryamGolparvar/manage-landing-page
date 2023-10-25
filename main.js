6838aac6ee042aabd73a94df350b232a78499e35
Maryam Golparvar <maryamgolparvar1@gmail.com>
2023-10-25 19:30:31 +0330

    hamburger menu added to mobile navigation bar

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  // primaryNav.classList.toggle("opened");
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+});
